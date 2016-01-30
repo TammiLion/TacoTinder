9cc68aefb8b02c6d02225483a6b45f6ffff8ddab
Thomas van Dam <t.thomas.van.dam@gmail.com>
2016-01-30 12:13:49 +0100

    Make more functions public.

--- TacoTinder/Assets/Scripts/Player.cs
+++ TacoTinder/Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ public class Player : MonoBehaviour {
 	public float baseCooldown;
 	public float baseSpeed;
 	public God god;
-	public Vector2 direction;
+	public Vector2 direction; // Maybe this should be private?
 //	public Weapon weapon;
 
 	private float cooldownTimeStamp;
@@ -15,7 +15,12 @@ public class Player : MonoBehaviour {
 		
 	}
 
-	void Fire () {
+	public void Move(float horizontal, float vertical) {
+		Vector2 moveDirection = new Vector2 (horizontal, vertical);
+
+	}
+
+	public void Fire () {
 		// Don't fire when the cooldown is still active.
 		if (this.cooldownTimeStamp >= Time.time) {
 			return;

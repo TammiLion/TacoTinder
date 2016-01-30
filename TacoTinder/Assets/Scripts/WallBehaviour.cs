45381bb81f8b24800781043d0e6ce37685a56acb
dimitris dimitriadis <melkorinos@gmail.com>
2016-01-30 21:57:21 +0100

    Spawn wall and WallBehaviour
    Assuming players have rotation towards center and tag Player

---
+++ TacoTinder/Assets/Scripts/WallBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallBehaviour : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Mob")
+        {
+            // Turn the mob around for now.
+            Moveable mobMoveable = other.GetComponent<Moveable>();
+            Vector2 mobDirection = mobMoveable.direction;
+            // Inverse the direction of the mob.
+            mobMoveable.direction = new Vector2(-1 * mobDirection.x + (Random.value / 2f), -1 * mobDirection.y + (Random.value / 1.5f));
+        }
+    }
+
+
+}

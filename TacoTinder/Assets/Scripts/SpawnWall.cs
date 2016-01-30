6f513cb35989f247103de81133ef8919b896fbe6
dimitris dimitriadis <melkorinos@gmail.com>
2016-01-30 22:40:05 +0100

    Hardcoded spawnWall and wallBehaviour

--- TacoTinder/Assets/Scripts/SpawnWall.cs
+++ TacoTinder/Assets/Scripts/SpawnWall.cs
@@ -3,41 +3,15 @@ using System.Collections;
 
 public class SpawnWall : MonoBehaviour
 {
-
-    public GameObject[] players;
-    public float timeToDestroy;
     public GameObject wall;
-    public float offsetRatio;
-
-    Vector3 offset;
-
-    void Start() { buildWall(); }
 
     void buildWall()
     {
-        Debug.Log("start");
-        players = GameObject.FindGameObjectsWithTag("Player");
-
-        foreach (GameObject player in players)
-        {
-            Debug.Log(player);
-            offset = Vector3.zero;
-            if (player.name != "inca")
-            {
-
-                if (player.transform.position.y == 0)
-                {
-                    offset.y -= player.transform.position.x / offsetRatio;
-                }
-                else {
-                    offset.x -= player.transform.position.y / offsetRatio;
-                }
-
-                GameObject newWall = Instantiate(wall, player.transform.position + offset, player.transform.rotation) as GameObject;
-                Destroy(newWall, timeToDestroy);
-            }
-        }
+        GameObject newWall1 = Instantiate(wall, new Vector3(-2.3f, 0, 0), Quaternion.Euler(0, 0, 90)) as GameObject;
+        GameObject newWall2 = Instantiate(wall, new Vector3(0, -2.3f, 0), Quaternion.identity) as GameObject;
+        GameObject newWall3 = Instantiate(wall, new Vector3(2.3f, 0, 0), Quaternion.Euler(0, 0, 90)) as GameObject;
     }
-}
 
+
+}
 

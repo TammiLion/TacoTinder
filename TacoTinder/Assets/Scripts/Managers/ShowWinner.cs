d7531d9f589b1e21cb9c0e9f1704a11c1bf31b40
dimitris dimitriadis <melkorinos@gmail.com>
2016-01-30 23:55:16 +0100

    Show winner ,
    has function that takes winning player as argument

---
+++ TacoTinder/Assets/Scripts/Managers/ShowWinner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowWinner : MonoBehaviour {
+
+    public Sprite winAquaman;
+    public Sprite winPyramid;
+    public Sprite winInca;
+    public Sprite winJapan;
+
+    Sprite winIMG;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+
+    void showWinScreen (Player player)
+    {
+        if (player.name == "aquaman") winIMG = winAquaman;
+        if (player.name == "pyramid") winIMG = winPyramid;
+        if (player.name == "inca") winIMG = winInca;
+        if (player.name == "japan") winIMG = winJapan;
+
+        Instantiate(winIMG, Vector3.zero, Quaternion.identity);
+    }
+	
+	
+}

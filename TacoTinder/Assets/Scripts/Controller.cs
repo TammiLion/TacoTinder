a1b29e72059958d448acc6e9c2e7df2765a2e388
Tamara Roep <trp300@gmail.com>
2016-01-30 02:11:53 +0100

    Added a TesMoveable object. Modified Controller Script.

--- TacoTinder/Assets/Scripts/Controller.cs
+++ TacoTinder/Assets/Scripts/Controller.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class Controller : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	//Controller 1 to 4
+	public int id = 1;
+	public Player player;
+
+	public const string PREFIX_JOYSTICK = "Joystick "; 
+
+	public Controller(int id, Player player) {
+		this.id = id;
+		this.player = player;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (player == null) {
+			return;
+		}
+		checkXAxis ();
+		checkAButton ();
+	}
+
+	private void checkXAxis() {
+		float value = Input.GetAxis (PREFIX_JOYSTICK + id + " X axis");
+		if (value != 0) {
+			//player.moveAim(value);
+		}
+	}
+
+	private void checkAButton() {
+		if (Input.GetButtonDown (PREFIX_JOYSTICK + id + " A button")) {
+			//player.shoot(valueAButton);
+		}
 	}
 }

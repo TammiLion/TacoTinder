ac656c4a4da0c20a0de95fd1ed910d5e8352f7d1
dimitris dimitriadis <melkorinos@gmail.com>
2016-01-30 16:11:41 +0100

    Adding Sponsor scene and script

---
+++ TacoTinder/Assets/Scripts/SponsorsScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SponsorsScreen : MonoBehaviour {
+
+    public int waitTime;
+
+    // Use this for initialization
+    void Start() {
+        StartCoroutine(ExecuteAfterTime(waitTime));
+    }
+
+    IEnumerator ExecuteAfterTime(int waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        SceneManager.LoadScene("CharacterSelectionMenu");
+
+    }
+}

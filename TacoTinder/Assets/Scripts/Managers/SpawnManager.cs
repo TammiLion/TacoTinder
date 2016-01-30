7be61cb3b835bbfa11b179fed5ced763cf0b6dd4
dimitris dimitriadis <melkorinos@gmail.com>
2016-01-30 10:03:11 +0100

    Dimitris tries unity.

---
+++ TacoTinder/Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject mob;
+    public Vector2[] spawnPositions;
+    public float cooldownTimer;
+    public float firstSpawnTime;
+ 
+    Vector2 vectorPos;
+    Vector2 dir;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("SpawnWaves", firstSpawnTime, cooldownTimer); 
+    }
+
+    void SpawnWaves()
+    {
+        //get a random spawn position
+        vectorPos = spawnPositions[Random.Range(0, 3)];
+
+        //et direction towards the center
+        dir = Vector2.zero - vectorPos;
+        dir.Normalize ();
+
+        //instantiate new mob
+        Mob newMob = Instantiate(mob, vectorPos , Quaternion.Euler(dir.x,0,dir.y) ) as Mob;
+        newMob.type = "rock";
+        newMob.isSuper = true;
+
+        //wait this many seconds until it runs again
+        
+    }
+}
--- TacoTinder/Assets/Scripts/Mob.cs
+++ TacoTinder/Assets/Scripts/Mob.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 public class Mob : MonoBehaviour {
 
+    public string type;
+    public bool isSuper;
+    public Player target;
+    public const float INITIAL_SPEED = 1;
+
+    int points;
+    float speed = INITIAL_SPEED;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +18,15 @@ public class Mob : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    //update the speed of the mob
 	}
+
+    int GetPoints()
+    {
+        if (isSuper) points += 10;
+
+        if (type = target.type) points ++;
+
+        return points;
+    }
 }

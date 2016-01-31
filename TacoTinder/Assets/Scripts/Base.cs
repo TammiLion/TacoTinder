7d648851bc796be4534f836c75f6fb467c99cf23
Thomas van Dam <t.thomas.van.dam@gmail.com>
2016-01-31 10:09:38 +0100

    Destroy projectiles when they collide with the base.

--- TacoTinder/Assets/Scripts/Base.cs
+++ TacoTinder/Assets/Scripts/Base.cs
@@ -9,10 +9,12 @@ public class Base : MonoBehaviour
 	
     void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.tag == "Mob") {
-			Debug.Log ("Mob was of God: " + coll.GetComponent<Mob>().god);
+		if (coll.tag == "Mob") {
+			Debug.Log ("Mob was of God: " + coll.GetComponent<Mob> ().god);
 			points += coll.GetComponent<Mob> ().GetPoints (player);
 			scoreText.text = "Score: " + points;
+			Destroy (coll.gameObject);
+		} else if (coll.tag == "Projectile") {
 			Destroy (coll.gameObject);
 		}
     }

abfefd8abf241fc1adc2f9d1197a640a69c120f0
dimitris dimitriadis <melkorinos@gmail.com>
2016-01-31 12:02:59 +0100

    Opening scene and script

---
+++ TacoTinder/Assets/Scripts/OpeningScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpeningScene : MonoBehaviour {
+
+
+    public int waitTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(ExecuteAfterTime(waitTime));
+    }
+
+    IEnumerator ExecuteAfterTime(int waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        //SceneManager.LoadScene("Sponsors");
+
+    }
+}

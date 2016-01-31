3d1f9b9f5d66df815043361efa6f566cdcc021bf
dimitris dimitriadis <melkorinos@gmail.com>
2016-01-31 13:23:34 +0100

    Wall prefab and wall spawner script

--- TacoTinder/Assets/Scripts/SpawnWall.cs
+++ TacoTinder/Assets/Scripts/SpawnWall.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class SpawnWall : MonoBehaviour
 {
-    public GameObject wall;
+    public GameObject wallPrefab;
+
+    void Update()
+    {
+        if (Input.anyKeyDown) buildWall(); 
+    }
 
     void buildWall()
     {
-        GameObject newWall1 = Instantiate(wall, new Vector3(-2.3f, 0, 0), Quaternion.Euler(0, 0, 90)) as GameObject;
-        GameObject newWall2 = Instantiate(wall, new Vector3(0, -2.3f, 0), Quaternion.identity) as GameObject;
-        GameObject newWall3 = Instantiate(wall, new Vector3(2.3f, 0, 0), Quaternion.Euler(0, 0, 90)) as GameObject;
+        //Wall left
+        GameObject wallLeft = Instantiate(wallPrefab, new Vector3(-2.64f, -0.06f, 0), Quaternion.Euler(0, 0, -0.66f)) as GameObject;
+        //Wall right
+        GameObject wallRight = Instantiate(wallPrefab, new Vector3(2.81f, -0.21f, 0), Quaternion.Euler(0, 0, 170.7f)) as GameObject;
+        //Wall bottom
+        GameObject wallBottom = Instantiate(wallPrefab, new Vector3(0.13f, -2.71f, 0), Quaternion.Euler(0, 0, 87.7f)) as GameObject;
+        //wallBottom.transform.localScale += new Vector3(0.6f, 0.4f, 0.6f);
     }
 
 
